bb68fb1218dbf86013276df039681915534a4343
simrasarfaraz <simra.sarfaraaz@gmail.com>
2024-07-23 20:46:15 +0530

    Data Types in Java practice

---
+++ PracticeProgram/src/DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //declare the variables with data types
+        int myAge = 34;
+        //size of long is greater than int
+        long mySalary = 150000;
+        //to store the Single Character Value
+        char myGender = 'M';
+        //to store any Text type value
+        String myName = "Pawan Sharma";
+        //to store the decimal value with larger size than float
+        double bankInterest = 3.08565;
+        //to store the decimal value
+        float roi = 2.5F;
+        //to store the true or false value
+        boolean hasGF = false;
+
+        //print all variable
+        System.out.println("My age is : "+myAge);
+        System.out.println("My salary is : "+mySalary);
+        System.out.println("My gender is : "+myGender);
+        System.out.println("My name is : "+myName);
+        System.out.println("My bank interest is : "+bankInterest);
+        System.out.println("My bank roi is : "+roi);
+        System.out.println("Do I have gf : "+ hasGF);
+    }
+}
