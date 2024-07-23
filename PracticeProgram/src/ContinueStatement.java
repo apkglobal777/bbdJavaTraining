b277217e30fbc303bdd41d7f963a423f8ece7c3d
simrasarfaraz <simra.sarfaraaz@gmail.com>
2024-07-24 01:56:23 +0530

    continue statement  in java

---
+++ PracticeProgram/src/ContinueStatement.java
@@ -0,0 +1,13 @@
+public class ContinueStatement {
+    public static void main(String[] args) {
+        //it will skip the current iteration
+        //e.g. print 10 natural no but 4 & 7 should not print
+        for (int i = 1; i < 10; i++) {
+            if (i == 4 || i == 7)
+            {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
