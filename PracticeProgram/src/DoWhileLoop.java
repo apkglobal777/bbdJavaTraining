bbb7b86f7bc2c5feb6549dc710dcff5398bde2d7
simrasarfaraz <simra.sarfaraaz@gmail.com>
2024-07-24 01:42:51 +0530

    do while loop in java

---
+++ PracticeProgram/src/DoWhileLoop.java
@@ -0,0 +1,13 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        //print 10 natural no from 1 to 10 and find the sum
+        int i = 1;
+        int sum = 0;
+        do {
+            System.out.println(i);
+            sum = sum + i;
+            i++;
+        }while (i <= 10);
+        System.out.println("The sum is: "+sum);
+    }
+}
