69d15b278636b064664f52074217a869d96a1b00
simrasarfaraz <simra.sarfaraaz@gmail.com>
2024-07-24 01:24:15 +0530

    Typecasting in java

---
+++ PracticeProgram/src/TypeCasting.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        //to convert one data type to another data type
+        //e.g petrol pump salesperson charge in int rupees
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your petrol exact charges ");
+        float exactPetrolCharges = scanner.nextFloat();
+        //convert float to int to pay in rupees
+        int paidPetrolCharges = (int) exactPetrolCharges;
+        System.out.println("Paid amount is : "+ paidPetrolCharges);
+    }
+}
