da31b8a132d5f1d80a8a7a2dbd4eba4b47a7ce5b
simrasarfaraz <simra.sarfaraaz@gmail.com>
2024-07-24 13:19:59 +0530

    Array in Java

---
+++ PracticeProgram/src/ArrayDemo.java
@@ -0,0 +1,25 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        String [] friendNames = {"Anshu", "Ivan", "Kunal", "Rahul", "Mudit"};
+
+        System.out.println(friendNames.length);
+        for (int i = 0; i < friendNames.length; i++) {
+            System.out.println(friendNames[i]);
+        }
+        friendNames[3] = "rini";
+        for (int i = 0; i < friendNames.length; i++) {
+            System.out.println(friendNames[i]);
+        }
+        //print the name of friends using for each loop
+        for (String i: friendNames)
+        {
+            System.out.println(i);
+        }
+
+        findEvenOddNumber(3);
+    }
+
+    private static void findEvenOddNumber(int i) {
+        
+    }
+}
