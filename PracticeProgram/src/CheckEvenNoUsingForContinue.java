8c041d5d85ce93b7060f6dda69ddeca226783923
simrasarfaraz <simra.sarfaraaz@gmail.com>
2024-07-24 11:43:02 +0530

    Check even no using for and continue

---
+++ PracticeProgram/src/CheckEvenNoUsingForContinue.java
@@ -0,0 +1,12 @@
+public class CheckEvenNoUsingForContinue {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0)
+            {
+                System.out.println("Even no"+ i);
+            }else {
+                System.out.println("Odd no"+i);
+            }
+        }
+    }
+}
